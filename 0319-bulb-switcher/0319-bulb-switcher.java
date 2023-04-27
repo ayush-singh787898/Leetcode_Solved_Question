c1c131325c893402fd11d4177bc56aedd9641156
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2023-04-27 22:31:37 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (68.86%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        double square_Root=Math.sqrt(n);
+            double d=Math.floor(square_Root);
+            int tb=(int)d;
+            return tb;
+        
+    }
+}
