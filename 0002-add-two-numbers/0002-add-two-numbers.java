80710c3db6725c7a4b219a89cd2919f56201ec76
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2023-07-02 00:17:57 +0530

    Time: 2 ms (99.43%), Space: 43.8 MB (12.80%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,91 +8,91 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//         List<Integer>al=new ArrayList<>();
-//         int sum=0;
-//         ListNode x=l1;
-//         ListNode y=l2;
-//         int carry=0;
-//         if(x==null && y==null){
-//             return null;
-//         }
-// // case-1 when x and y are not null
-        
-//         while(x!=null && y!=null){
-//             sum=x.val+y.val+carry;
-//             if(sum>9){
-//                 int n=sum;
-//                 while(n>9){
-//                     int temp=n%10;
-//                     n=n/10;
-//                     al.add(temp);
-//                     carry=n;   
-//                 }
-//             }
-//             else{
-//                 al.add(sum);
-//             }
-//             x=x.next;
-//             y=y.next;
-//         }
-
-        
-
-//         System.out.println(al);
-//         return l1;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry =0;
-        ListNode newHead = new ListNode(0);
-        ListNode p1 = l1, p2 = l2, p3=newHead;
-        while(p1 != null || p2 != null)
-        {
-            if(p1 != null)
-            {
-                carry += p1.val;
-                p1 = p1.next;
-            }
-            if(p2 != null)
-            {
-                carry += p2.val;
-                p2 = p2.next;
-            }
-            p3.next = new ListNode(carry%10);
-            p3 = p3.next;
-            carry /= 10;
+        int sum=0;
+        ListNode x=l1;
+        ListNode y=l2;
+        ListNode newNode=new ListNode(0);
+        ListNode z=newNode;
+        int carry=0;
+        if(x==null && y==null){
+            return null;
         }
-        if(carry==1)
-            p3.next=new ListNode(1);
-        return newHead.next;
+        
+        while(x!=null || y!=null){
+            sum+=carry;
+            if(x!=null){
+                sum+=x.val;
+                x=x.next;
+                
+            }
+            if(y!=null){
+                sum+=y.val;
+                y=y.next;
+            }
+            z.next=new ListNode(sum%10);
+            z=z.next;
+            sum=sum/10;
+        
+        }
+        if(sum==1){
+            z.next=new ListNode(sum);
+        }
+        return newNode.next;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         int carry =0;
+        // ListNode newHead = new ListNode(0);
+//         ListNode p1 = l1, p2 = l2, p3=newHead;
+//         while(p1 != null || p2 != null)
+//         {
+//             if(p1 != null)
+//             {
+//                 carry += p1.val;
+//                 p1 = p1.next;
+//             }
+//             if(p2 != null)
+//             {
+//                 carry += p2.val;
+//                 p2 = p2.next;
+//             }
+//             p3.next = new ListNode(carry%10);
+//             p3 = p3.next;
+//             carry /= 10;
+//         }
+//         if(carry==1)
+//             p3.next=new ListNode(1);
+//         return newHead.next;
+//     }
+// }
