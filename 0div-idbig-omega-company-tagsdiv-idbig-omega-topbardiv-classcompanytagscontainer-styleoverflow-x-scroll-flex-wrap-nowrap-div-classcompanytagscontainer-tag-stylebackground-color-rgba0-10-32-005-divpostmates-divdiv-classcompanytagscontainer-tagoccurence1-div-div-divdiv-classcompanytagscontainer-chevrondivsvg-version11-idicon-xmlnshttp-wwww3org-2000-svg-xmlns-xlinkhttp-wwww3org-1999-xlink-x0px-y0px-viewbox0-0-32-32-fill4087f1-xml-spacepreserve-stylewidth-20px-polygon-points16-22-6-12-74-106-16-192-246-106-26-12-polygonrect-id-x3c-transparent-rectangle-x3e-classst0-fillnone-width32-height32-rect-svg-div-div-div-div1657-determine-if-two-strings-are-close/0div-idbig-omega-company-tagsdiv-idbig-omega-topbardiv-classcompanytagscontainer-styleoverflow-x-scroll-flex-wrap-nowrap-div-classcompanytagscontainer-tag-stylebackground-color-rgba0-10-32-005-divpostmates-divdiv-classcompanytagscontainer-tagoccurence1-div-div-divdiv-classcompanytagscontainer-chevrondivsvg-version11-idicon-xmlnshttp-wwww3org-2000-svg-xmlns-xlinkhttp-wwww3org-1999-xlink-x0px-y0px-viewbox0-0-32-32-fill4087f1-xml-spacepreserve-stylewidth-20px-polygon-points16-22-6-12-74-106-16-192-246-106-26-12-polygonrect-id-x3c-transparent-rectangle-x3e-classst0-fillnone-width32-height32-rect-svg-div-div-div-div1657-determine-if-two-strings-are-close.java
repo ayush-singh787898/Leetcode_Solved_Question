c7d894b0c36bed9772c6238c18f9dec1c472e170
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-01-17 09:36:07 +0530

    Time: 10 ms (76.77%), Space: 45.6 MB (48.30%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divpostmates-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1657-determine-if-two-strings-are-close/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divpostmates-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1657-determine-if-two-strings-are-close.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divpostmates-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1657-determine-if-two-strings-are-close/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divpostmates-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1657-determine-if-two-strings-are-close.java
@@ -1,27 +1,29 @@
+
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length()) {
+        if(word1.length()!=word2.length())
             return false;
-        }
-
-        int arr1[]=new int[26];
-        int arr2[]=new int[26];
-        for(char ch:word1.toCharArray()){
-            arr1[ch-'a']++;
-        }
-        for(char ch:word2.toCharArray()){
-            arr2[ch-'a']++;
-        }
-        for(int i=0;i<arr1.length;i++){
-            if((arr1[i]==0 && arr2[i]!=0) || arr2[i]==0 && arr1[i]!=0){
+        
+        int arr1[] = new int[26];
+        int arr2[] = new int[26];
+        
+        for(int i=0;i<word1.length();i++)
+            arr1[word1.charAt(i)-'a']++;
+        
+        for(int i=0;i<word2.length();i++)
+            arr2[word2.charAt(i)-'a']++;
+        
+        for(int i=0;i<26;i++)
+            if((arr1[i]==0 && arr2[i]!=0) || (arr1[i]!=0 && arr2[i]==0))
                 return false;
-            }
-        }
+        
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-        return Arrays.equals(arr1,arr2);
+        
+        for(int i=0;i<26;i++)
+            if(arr1[i]!=arr2[i])
+                return false;
+        
+        return true;
     }
 }
-
-
-
