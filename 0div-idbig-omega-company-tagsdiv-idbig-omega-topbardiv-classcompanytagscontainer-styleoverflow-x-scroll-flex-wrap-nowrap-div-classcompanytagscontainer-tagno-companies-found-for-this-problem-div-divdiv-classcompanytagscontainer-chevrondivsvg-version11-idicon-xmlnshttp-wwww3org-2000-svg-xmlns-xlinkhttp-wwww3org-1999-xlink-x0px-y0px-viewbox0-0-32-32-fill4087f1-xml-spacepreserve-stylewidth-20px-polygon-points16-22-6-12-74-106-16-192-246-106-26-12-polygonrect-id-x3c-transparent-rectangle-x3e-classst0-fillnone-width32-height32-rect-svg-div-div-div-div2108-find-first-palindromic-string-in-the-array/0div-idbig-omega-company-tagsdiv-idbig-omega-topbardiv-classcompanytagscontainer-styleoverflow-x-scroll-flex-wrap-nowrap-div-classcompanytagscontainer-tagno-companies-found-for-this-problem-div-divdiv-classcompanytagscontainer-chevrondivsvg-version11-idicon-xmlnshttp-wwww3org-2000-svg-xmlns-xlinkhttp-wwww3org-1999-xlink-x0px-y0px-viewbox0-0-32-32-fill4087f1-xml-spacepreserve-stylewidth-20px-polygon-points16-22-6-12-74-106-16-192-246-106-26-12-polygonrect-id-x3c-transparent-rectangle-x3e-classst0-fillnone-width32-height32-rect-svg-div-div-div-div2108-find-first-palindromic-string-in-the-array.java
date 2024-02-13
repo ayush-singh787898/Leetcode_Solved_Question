d221c03e4fb1074a7c3e8e1b67dc7bee1c39cb5b
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-02-13 08:33:40 +0530

    Time: 4 ms (40.56%), Space: 44.7 MB (76.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array.java
@@ -1,42 +1,12 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-        boolean isPalindrome=false;
-        String str="";
-        for(int i=0;i<words.length;i++){
-            int left=0;
-            
-            int right=words[i].length()-1;
-            if(words[i].length()%2!=0){
-                while(left<=right){
-                    if(words[i].charAt(left)!=words[i].charAt(right)){
-                        break;
-                    }
-                    System.out.println(i);
-                    if(left==right){
-                        str+=words[i];
-                        return str;
-                    }
-                    left++;
-                    right--;
-                }
+        for(String word:words){
+            StringBuilder reverseString = new StringBuilder(word).reverse();
+            if(word.equals(reverseString.toString())){
+                return word;
             }
-            if(words[i].length()%2==0){
-                
-            
-                while(left<right){
-                    if(words[i].charAt(left)!=words[i].charAt(right)){
-                        break;
-                    }
-                    System.out.println(i);
-                    if(left==words[i].length()/2-1 && right==words[i].length()/2){
-                        str+=words[i];
-                        return str;
-                    }
-                    left++;
-                    right--;
-                }
-            }    
         }
-        return str;
+        
+        return "";
     }
 }
