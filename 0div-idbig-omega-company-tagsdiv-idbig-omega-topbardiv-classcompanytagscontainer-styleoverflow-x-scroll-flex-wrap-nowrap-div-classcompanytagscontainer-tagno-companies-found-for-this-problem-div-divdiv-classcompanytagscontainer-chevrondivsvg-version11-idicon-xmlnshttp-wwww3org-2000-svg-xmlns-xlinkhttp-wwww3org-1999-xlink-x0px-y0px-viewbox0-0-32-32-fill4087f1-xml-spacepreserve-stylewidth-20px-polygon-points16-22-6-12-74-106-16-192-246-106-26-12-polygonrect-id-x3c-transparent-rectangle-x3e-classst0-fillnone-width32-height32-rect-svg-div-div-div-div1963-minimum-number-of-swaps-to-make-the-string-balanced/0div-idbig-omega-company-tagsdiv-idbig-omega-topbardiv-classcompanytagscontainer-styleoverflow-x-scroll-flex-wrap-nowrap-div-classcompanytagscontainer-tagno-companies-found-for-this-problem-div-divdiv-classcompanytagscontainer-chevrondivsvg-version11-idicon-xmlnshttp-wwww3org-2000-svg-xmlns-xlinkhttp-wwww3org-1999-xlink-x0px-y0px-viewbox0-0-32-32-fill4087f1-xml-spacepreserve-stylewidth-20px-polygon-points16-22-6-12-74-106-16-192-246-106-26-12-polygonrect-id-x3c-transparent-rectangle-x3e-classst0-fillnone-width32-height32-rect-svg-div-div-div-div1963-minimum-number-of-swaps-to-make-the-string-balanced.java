345705795f588a615d1a6bb1ec8dc1b98cc0dbbf
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-04-06 10:20:26 +0530

    Time: 17 ms (49.52%), Space: 54 MB (72.31%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1963-minimum-number-of-swaps-to-make-the-string-balanced/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minSwaps(String s) {
+        int open=0;
+        int close=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='['){
+                open++;
+            }
+            else{
+                if(open<=0){
+                    close++;
+                }
+                else{
+                    open--;
+                }
+            }
+        }
+        return (close+1)/2;
+    }
+}
