4c4ba09f287541138d60236aee050637468afda9
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-07-17 11:03:50 +0530

    Time: 11 ms (35.99%), Space: 41 MB (43.45%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,44 @@
+
+
+
+class Solution {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+                    return false; // trigger backtracking
+                }
+            }
+        }
+        return true; // puzzle solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+            // Check column
+            if (board[i][col] == c) return false;
+            // Check 3x3 box
+            if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
