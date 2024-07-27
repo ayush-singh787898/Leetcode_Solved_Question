9178b28f38f1b2f1a576db090b76fe8489d90c4c
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-07-27 07:17:25 +0530

    Time: 86 ms (22.26%), Space: 77.4 MB (15.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,4 @@
 class Solution {
-    public static int longestIncr(int i,int[]num,int n,int prev,int[][]dp){
-        if(i==n){   
-        }
-        if(dp[i][prev+1]!=-1){
-            return dp[i][prev+1];
-        }
- 
-        //not Taking
-        int len=0+longestIncr(i+1,num,n,prev,dp);
-        // Taking
-        if(prev==-1 || num[i]>num[prev]){
-            len=Math.max(len,1+longestIncr(i+1,num,n,i,dp));
-        }
-        return dp[i][prev+1]=len;
-    }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
         int dp[][]=new int[n+1][n+1];
@@ -27,7 +12,7 @@ class Solution {
             }
 
         }   
-                return dp[0][0];
+     return dp[0][0];
 
     }
 }
