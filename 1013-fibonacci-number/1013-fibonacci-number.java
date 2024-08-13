61697381e13adce0b77c1d63724feac967115b0d
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-13 23:56:40 +0530

    Time: 0 ms (100%), Space: 40.1 MB (60.75%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -20,27 +20,59 @@
 //     }
 // }
 
-class Solution {
-    public static int fibonacci(int n,int dp[]){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int sum=fibonacci(n-2,dp)+fibonacci(n-1,dp);
-        return sum;
-    }
+
+
+
+// ================= dp memoization  ======================
+
+// class Solution {
+//     public static int fibonacci(int n,int dp[]){
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         int sum=fibonacci(n-2,dp)+fibonacci(n-1,dp);
+//         return sum;
+//     }
     
 
-    public int fib(int n) {
+//     public int fib(int n) {
 
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fibonacci(n,dp);
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return fibonacci(n,dp);
 
-    }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+// =================== dp  tabulation ==============================
+
+class Solution {
+    public static int fib(int n){
+        if(n<2){
+            return n;
+        }
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]+=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }    
 }
