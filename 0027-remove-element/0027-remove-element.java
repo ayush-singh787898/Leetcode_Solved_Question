acef32a42a9b3f2216d0d89c7ecbecb5db1e76ae
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-13 12:36:55 +0530

    Time: 0 ms (100%), Space: 42 MB (16.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        Stack<Integer>stk=new Stack<>();
         int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                nums[k]=nums[i];
-                k++;
-                
+                stk.push(nums[i]);
+                nums[k++]=nums[i];
             }
         }
-        return k;
+        return stk.size();
     }
 }
