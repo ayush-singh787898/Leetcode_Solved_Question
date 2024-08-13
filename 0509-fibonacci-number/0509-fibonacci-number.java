12f283e062af2c5f794a42555dc5ccc1ad0f41c2
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-13 23:42:42 +0530

    Time: 10 ms (9.92%), Space: 40.1 MB (76.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,40 @@
+// Iterative method
+
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+//         int a=0;
+//         int b=1;
+//         int sum=0;
+//         for(int i=1;i<n;i++){
+//             sum=a+b;
+//             a=b;
+//             b=sum;
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
-    public int fib(int n) {
+    public static int fibonacci(int n){
         if(n==0){
             return 0;
         }
         if(n==1){
             return 1;
         }
-        int a=0;
-        int b=1;
-        int sum=0;
-        for(int i=1;i<n;i++){
-            sum=a+b;
-            a=b;
-            b=sum;
-        }
+        int sum=fibonacci(n-2)+fibonacci(n-1);
         return sum;
+    }
+    
+
+    public int fib(int n) {
+
+        return fibonacci(n);
     }
 }
