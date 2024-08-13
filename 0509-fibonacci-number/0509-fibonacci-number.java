60e992042d890f6304ba9432a83b08f899aa2703
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-13 23:31:44 +0530

    Time: 0 ms (100%), Space: 40 MB (76.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,19 @@
 class Solution {
-    public static int fibonacci(int n){
-        if(n==0 || n==1){
-            return n;
-        }
-        int ans=fibonacci(n-1)+fibonacci(n-2);
-        return ans;
-    }
     public int fib(int n) {
-        return fibonacci(n);
-        
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int a=0;
+        int b=1;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
     }
 }
