c5ea64af84d2bfbfa202d11f0e1feebda5ea1220
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-20 23:14:20 +0530

    Time: 134 ms (6.24%), Space: 45.8 MB (11.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,41 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String x=s.toLowerCase();
-        Set<Character>set=new HashSet<>();
-        Set<Integer>Nset=new HashSet<>();
-        for(char ch='a';ch<'z';ch++){
-            set.add(ch);
-        }
-        for(int i=0;i<=9;i++){
-            Nset.add(i);
-        }
         String str="";
-        for(int i=0;i<x.length();i++){
-            if(set.contains(x.charAt(i))){
-                str+=x.charAt(i);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' ' && s.charAt(i)!=',' && s.charAt(i)!=':' && Character.isLetterOrDigit(s.charAt(i))){
+                str+=s.charAt(i);
             }
-  
-            if (Nset.contains(x.charAt(i) - '0')) {
-                 str += x.charAt(i);
-            }
-
         }
-        int left=0;
-        int right=str.length()-1;
-        boolean flag=true;
-         while(left<right){
-            if(str.charAt(left)!=str.charAt(right)){
-                flag=false;
+        str=str.toLowerCase();
+        int i=0;
+        int j=str.length()-1;
+        System.out.print(str);
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
             }
-            left++;
-            right--;
-         }
-        if(flag){
-            return true;
+            i++;
+            j--;
         }
-        System.out.println(str);
-   
-
-        return false;
+        return true;
     }
 }
