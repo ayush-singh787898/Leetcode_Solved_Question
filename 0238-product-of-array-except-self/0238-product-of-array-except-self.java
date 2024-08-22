634701d2545e6d814997eea230ad159dc336499f
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-23 00:00:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,89 +1,17 @@
-
-
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int arr[]=new int[nums.length];
-        int rp=1;
-        // for left part
+        int x=0;
         for(int i=0;i<nums.length;i++){
-            arr[i]=rp;
-            rp=rp*nums[i];
-        }
-        // for right par
-        rp=1;
-        for(int i=nums.length-1;i>=0;i--){
-            arr[i]=arr[i]*rp;
-            rp=rp*nums[i];
+            int inx=0;       
+            int product=1;
+            for(int j=0;j<nums.length;j++){
+                if(i!=j){
+                    product=product*nums[j]; 
+                }
+            }
+            arr[i]=product;
         }
         return arr;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public int[] productExceptSelf(int[] nums) {
-//         int arr[]=new int[nums.length];
-   
-//         int j=0;
-//         int i=0;
-//         arr[i]=1;
-//         int temp=1;
-//         while(i<nums.length-1 && j<nums.length){
-//               if(i==nums.length-1){
-//                 arr[i]=1;
-//                 i=0;
-//                 j++;
-//             }
-//                 if(i==j){
-//                     // arr[i]=1;
-//                        temp=1; 
-//                     // continue;
-//                 }
-            
-         
-//                         System.out.println(arr[i]);
-//             // int temp=arr[i+1];
-//             arr[i]=temp*nums[i+1];
-//             temp=arr[i];
-//             System.out.println(arr[i]);
-
-//             // if(i==nums.length-1){
-//             //     arr[i]=1;
-//             //     i=0;
-//             //     j++;
-//             // }
-//             i++;
-//             System.out.println(Arrays.toString(arr));
-//         }
-//         return arr; 
-//     }
-// }
-
-
-
-
-
-
-
