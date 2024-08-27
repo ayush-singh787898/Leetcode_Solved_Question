ce60d4353e0290ba93a6332f67f373bc0abc1a99
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-08-28 00:01:48 +0530

    Time: 1 ms (87.73%), Space: 41.3 MB (87.46%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -1,37 +1,11 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        if(s.length()<3){
-            return 0;
-        }
-        List<String>lst=new ArrayList<>();
-        String str="";
         int count=0;
-        for(int i=0;i<s.length();i++){
-            if(count==3){
-                count=2;
-                lst.add(str);
-                System.out.println(str);
-                str=str.substring(1);
-            }
-            count++;
-            str+=s.charAt(i);
-        }
-        str+=s.charAt(s.length()-1);
-        lst.add(str);
-        int res=0;
-        for(int i=0;i<lst.size();i++){
-            Set<Character>set=new HashSet<>();
-            int c=0;
-            for(int j=0;j<3;j++){
-                if(!set.contains(lst.get(i).charAt(j))){
-                    set.add(lst.get(i).charAt(j));
-                    c++;
-                }
-            }
-            if(c==3){
-                res++;
+        for(int i=0;i<=s.length()-3;i++){
+            if(s.charAt(i)!=s.charAt(i+1) && s.charAt(i)!=s.charAt(i+2) && s.charAt(i+1)!=s.charAt(i+2)){
+                count++;
             }
         }
-        return res;
+        return count;
     }
 }
