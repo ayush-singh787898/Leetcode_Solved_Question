47405a7ec64048df3ca95d98d4625d4e7b8c339b
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-09-07 22:00:09 +0530

    Time: 1084 ms (5.71%), Space: 53.2 MB (30.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,43 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-    // Map<Integer,Integer>hm=new HashMap<>();
-    //     for(int i=0;i<nums.length;i++){
-    //         hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-    //     }
-    //     for(Map.Entry<Integer,Integer>entry:hm.entrySet()){
-    //         if(entry.getValue()>nums.length/2){
-    //             return entry.getKey();
-    //         }
-    //     }
-    //     return -1;
+       
+    class Solution {
+        public int majorityElement(int[] nums) {
+            
+            
+//             Method-1
+            for(int i=0;i<nums.length;i++){
+                int count=1;
+                for(int j=i+1;j<nums.length;j++){
+                    if(nums[i]==nums[j]){
+                        count++;
+                    }
+                }
+                if(count>nums.length/2){
+                    return nums[i];
+                }
+            }
+            return -1;
+            
+            
+            //             Method-2
+            
+
+            // Map<Integer,Integer>hm=new HashMap<>();
+            // for(int i=0;i<nums.length;i++){
+            //     hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+            // }
+            // for(Map.Entry<Integer,Integer>entry:hm.entrySet()){
+            //     if(entry.getValue()>nums.length/2){
+            //         return entry.getKey();
+            //     }
+            // }
+            // return -1;
 
 
 
-    //      Method-2
+    //      Method-3
 
-    Arrays.sort(nums);
-    return nums[nums.length/2];
-        
+            // Arrays.sort(nums);
+            // return nums[nums.length/2];
+
     }
-}
+       }
