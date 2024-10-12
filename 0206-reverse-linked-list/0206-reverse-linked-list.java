f90cbe0530e92448dca9bf1ddd61a864283375b9
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2024-10-12 09:55:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (82.27%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -9,17 +9,29 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode curr=head;
-        ListNode prev=null;
-        ListNode next=null;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+    
+    // Recursive function to reverse the list
+    
+    public static ListNode recursiveReverse(ListNode head, ListNode curr, ListNode prev) {
+        // Base case: if we reach the end of the list, return the previous node as the new head
+        if (curr == null) {
+            head = prev;
+            return head;
         }
-        head=prev;
-        return head;
+        
+        ListNode next = curr.next;  // Store the next node
+        curr.next = prev;           // Reverse the current node's pointer
+        
+        // Recursive call with updated pointers
+        return recursiveReverse(head, next, curr);
+    }
+
+    // Function to initiate the recursive reversal
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        
+        // Start recursion with the initial head, current, and previous pointers
+        return recursiveReverse(head, curr, prev);
     }
 }
