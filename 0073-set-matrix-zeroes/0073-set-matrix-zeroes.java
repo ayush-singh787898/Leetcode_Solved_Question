1fd74e6e4d30b9b3df3c2b11a88c28dc58ed387e
Ayush Kumar Singh <100115446+ayush-singh787898@users.noreply.github.com>
2025-06-28 08:53:14 +0530

    Time: 1 ms (72.29%), Space: 46 MB (15.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,31 +1,59 @@
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int arr[][]=new int[matrix.length][matrix[0].length];
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 arr[i][j]=matrix[i][j];
+//             }
+//         }
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matrix[i][j]==0){
+
+//                     // set col value to zero but in same row 
+//                     for(int k=0;k<matrix[0].length;k++){
+//                         arr[i][k]=0;
+//                     }
+
+//                     // set row value to zero but in same col
+//                     for(int k=0;k<matrix.length;k++){
+//                         arr[k][j]=0;
+//                     }
+//                 }   
+//             }
+//         }
+
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 matrix[i][j]=arr[i][j];
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
+
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int arr[][]=new int[matrix.length][matrix[0].length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                arr[i][j]=matrix[i][j];
-            }
-        }
+        boolean row[]=new boolean[matrix.length];
+        boolean col[]=new boolean[matrix[0].length];
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-
-                    // set col value to zero but in same row 
-                    for(int k=0;k<matrix[0].length;k++){
-                        arr[i][k]=0;
-                    }
-
-                    // set row value to zero but in same col
-                    for(int k=0;k<matrix.length;k++){
-                        arr[k][j]=0;
-                    }
-                }   
+                    row[i]=true;
+                    col[j]=true;
+                }
             }
         }
-
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                matrix[i][j]=arr[i][j];
+                if(row[i] || col[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
